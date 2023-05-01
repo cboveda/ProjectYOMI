439a696ad353daac5216f7645567d1c85163df11
Chris Boveda <cboveda@asu.edu>
2023-05-01 14:47:04 -0700

    Update project structure and set up script AssemblyDefinition in preparation for testing

---
+++ Assets/Tests/PlayerTests.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+public class NewTestScript
+{
+    // A Test behaves as an ordinary method
+    [Test]
+    public void NewTestScriptSimplePasses()
+    {
+        Assert.Equals(CharacterMove.Type.Special, 1);// Use the Assert class to test conditions
+    }
+
+    // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+    // `yield return null;` to skip a frame.
+    [UnityTest]
+    public IEnumerator NewTestScriptWithEnumeratorPasses()
+    {
+        // Use the Assert class to test conditions.
+        // Use yield to skip a frame.
+        yield return null;
+    }
+}
