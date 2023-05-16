ab373e118dfffca6ce99e4d538e0f3ad4d664ed0
Chris Boveda <cboveda@asu.edu>
2023-05-15 22:35:07 -0700

    Add ComboPath data script

---
+++ Assets/Scripts/Core/Data/Combo/ComboPath.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewCharacterComboPath", menuName = "Characters/Combo Path")]
+public class ComboPath : ScriptableObject
+{
+
+}
