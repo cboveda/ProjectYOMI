27fecfa498e2d01d474e7c29783e28e248c14ec8
Chris Boveda <cboveda@asu.edu>
2023-05-16 12:21:13 -0700

    Update combo path to utilize Database.MoveInteractions, and add to global installer for injection

--- Assets/Scripts/Core/Data/Combo/ComboPath.cs
+++ Assets/Scripts/Core/Data/Combo/ComboPath.cs
@@ -1,17 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using Zenject;
 
 [CreateAssetMenu(fileName = "NewCharacterComboPath", menuName = "Characters/Combo Path")]
 public class ComboPath : ScriptableObject
 {
     [SerializeField] private Move.Type _comboMove;
     [SerializeField] private Move.Type _freshComboMove;
-    [SerializeField] private Move.Type[] _mixUp;
-    [SerializeField] private Move.Type[] _freshMixUp;
+    
+    private IDatabase _database;
+
+    [Inject]
+    public void Construct(IDatabase database)
+    {
+        _database = database;
+    }
 
     public Move.Type ComboMove { get => _comboMove; }
     public Move.Type FreshComboMove { get => _freshComboMove; }
-    public Move.Type[] MixUp { get => _mixUp; }
-    public Move.Type[] FreshMixUp { get => _freshMixUp; }
+    public Move.Type[] MixUp { get => GetMixUpsForComboMove(_comboMove); }
+    public Move.Type[] FreshMixUp { get => GetMixUpsForComboMove(_freshComboMove); }
+
+    private Move.Type[] GetMixUpsForComboMove(Move.Type comboMove)
+    {
+        if (_database == null)
+        {
+            Debug.LogError("Please add ComboPath scriptable object to the container for injection.");
+            return null;
+        }
+
+        //Find the counters to the counter of the specified combo move.
+        List<Move.Type> output = new();
+        _database.MoveInteractions.DefeatsType(comboMove).ForEach(l => output.AddRange(_database.MoveInteractions.DefeatsType(l)));
+        return new HashSet<Move.Type>(output).ToArray();
+    }
 }
--- Assets/Scripts/Core/Installers/GlobalInstaller.cs
+++ Assets/Scripts/Core/Installers/GlobalInstaller.cs
@@ -1,10 +1,18 @@
 using Unity.Netcode;
+using UnityEngine;
 using Zenject;
 
 public class GlobalInstaller : MonoInstaller
 {
+    [SerializeField] private ScriptableObject[] _objects;
+
     public override void InstallBindings()
     {
+        foreach(var obj in _objects) 
+        { 
+            Container.QueueForInject(obj);
+        }
+
         Container.Bind<NetworkManager>()
             .FromComponentInHierarchy()
             .AsSingle()
