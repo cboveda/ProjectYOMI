22ad0739d955b54b5b7b8634b76513c98a09d8a2
Chris Boveda <cboveda@asu.edu>
2023-05-18 07:04:41 -0700

    Add combo evaluation to CombatEvaluator

--- Assets/Scripts/Gameplay/CombatEvaluator.cs
+++ Assets/Scripts/Gameplay/CombatEvaluator.cs
@@ -13,9 +13,11 @@ public class CombatEvaluator
     [SerializeField] private float _baseSpecialGain = 25f;
     [SerializeField] private float _chipDamageModifier = 0.5f;
     [SerializeField] private float _specialGainOnLossModifier = 0.35f;
+    [SerializeField] private float _comboDamageMultiplier = 1.5f;
 
     private IDatabase _database;
     private IPlayerDataCollection _players;
+    private ITurnHistory _turnHistory;
     private readonly List<CombatCommandBase> _combatCommands;
     private int _turnNumber = 0;
 
@@ -29,10 +31,11 @@ public class CombatEvaluator
     }
 
     [Inject]
-    public void Construct(IDatabase database, IPlayerDataCollection playerDataCollection)
+    public void Construct(IDatabase database, IPlayerDataCollection playerDataCollection, ITurnHistory turnHistory)
     {
         _database = database;
         _players = playerDataCollection;
+        _turnHistory = turnHistory;
     }
 
     public virtual void AddCombatCommand(CombatCommandBase combatCommand)
@@ -73,7 +76,84 @@ public class CombatEvaluator
             (movePlayer2 && _database.MoveInteractions.DefeatedByType(movePlayer2.MoveType).Contains(movePlayer1.MoveType));
 
         // Check combo context
-        // TODO
+        bool player1IsInCombo = playerCharacter1.ComboCount > 0;
+        bool player2IsInCombo = playerCharacter2.ComboCount > 0;
+        ComboType player1ComboType = ComboType.Normal;
+        ComboType player2ComboType = ComboType.Normal;
+
+        if (player1IsInCombo && TurnNumber > 0)
+        {
+            var lastMovePlayer1 = _database.Moves.GetMoveById(_turnHistory.TurnDataList[^1].PlayerData1.Action);
+            if (playerCharacter1.Character.ComboPathSet.TryGetValue(lastMovePlayer1.MoveType, out var player1ComboPath))
+            {
+
+                if (playerCharacter1.ComboIsFresh)
+                {
+                    if (player1ComboPath.FreshComboMove == movePlayer1.MoveType)
+                    {
+                        player1ComboType = ComboType.Combo;
+                        playerCharacter1.ComboIsFresh = false;
+                    }
+                    else if (player1ComboPath.FreshMixUp.Contains<Move.Type>(movePlayer1.MoveType))
+                    {
+                        player1ComboType = ComboType.MixUp;
+                        playerCharacter1.ComboIsFresh = true;
+                    }
+                    else
+                    {
+                        playerCharacter1.ComboIsFresh = true;
+                    }
+                }
+                else
+                {
+                    if (player1ComboPath.ComboMove == movePlayer1.MoveType)
+                    {
+                        player1ComboType = ComboType.Combo;
+                        playerCharacter2.ComboIsFresh = false;
+                    }
+                    else if (player1ComboPath.MixUp.Contains<Move.Type>(movePlayer1.MoveType))
+                    {
+                        player1ComboType = ComboType.MixUp;
+                        playerCharacter2.ComboIsFresh = true;
+                    }
+                    else
+                    {
+                        playerCharacter2.ComboIsFresh = true;
+                    }
+                }
+            }
+        }
+
+        if (player2IsInCombo && TurnNumber > 0)
+        {
+            var lastMovePlayer2 = (TurnNumber > 0) ? _database.Moves.GetMoveById(_turnHistory.TurnDataList[^1].PlayerData2.Action) : null;
+            if (playerCharacter2.Character.ComboPathSet.TryGetValue(lastMovePlayer2.MoveType, out var player2ComboPath))
+            {
+
+                if (playerCharacter2.ComboIsFresh)
+                {
+                    if (player2ComboPath.FreshComboMove == movePlayer2.MoveType)
+                    {
+                        player2ComboType = ComboType.Combo;
+                    }
+                    else if (player2ComboPath.FreshMixUp.Contains<Move.Type>(movePlayer2.MoveType))
+                    {
+                        player2ComboType = ComboType.MixUp;
+                    }
+                }
+                else
+                {
+                    if (player2ComboPath.ComboMove == movePlayer2.MoveType)
+                    {
+                        player2ComboType = ComboType.Combo;
+                    }
+                    else if (player2ComboPath.MixUp.Contains<Move.Type>(movePlayer2.MoveType))
+                    {
+                        player2ComboType = ComboType.MixUp;
+                    }
+                }
+            }
+        }
 
         // Check for specials   
         if ((movePlayer1 != null) && (movePlayer1.MoveType == Move.Type.Special))
@@ -81,22 +161,29 @@ public class CombatEvaluator
             playerCharacter1.Effect.DoSpecial(player1Wins && !player2Wins);
             playerCharacter1.SpecialMeter = 0f;
             special1 = 0;
+            playerCharacter1.ComboIsFresh = true;
         }
 
         if ((movePlayer2 != null) && (movePlayer2.MoveType == Move.Type.Special))
         {
             playerCharacter2.Effect.DoSpecial(!player1Wins && player2Wins);
             playerCharacter2.SpecialMeter = 0f;
-
             special2 = 0;
+            playerCharacter1.ComboIsFresh = true;
         }
 
         // Check who won and apply updates
         if (player1Wins && !player2Wins)
         {
-            damageToPlayer2 = _baseDamage;
             positionChangePlayer1 = 1;
             positionChangePlayer2 = -1;
+            damageToPlayer2 = _baseDamage;
+            damageToPlayer2 *= player1ComboType switch
+            {
+                ComboType.Combo => _comboDamageMultiplier,
+                ComboType.MixUp => _comboDamageMultiplier,
+                _ => 1.0f
+            };
             special2 *= _specialGainOnLossModifier;
             playerCharacter1.IncrementComboCount();
             playerCharacter2.ResetComboCount();
@@ -106,6 +193,12 @@ public class CombatEvaluator
             positionChangePlayer1 = -1;
             positionChangePlayer2 = 1;
             damageToPlayer1 = _baseDamage;
+            damageToPlayer1 *= player2ComboType switch
+             {
+                 ComboType.Combo => _comboDamageMultiplier,
+                 ComboType.MixUp => _comboDamageMultiplier,
+                 _ => 1.0f
+             };
             special1 *= _specialGainOnLossModifier;
             playerCharacter1.ResetComboCount();
             playerCharacter1.IncrementComboCount();
