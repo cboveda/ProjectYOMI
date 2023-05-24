c99d2cdc0217a0cf6019d449dd5d131c279a0769
Chris Boveda <cboveda@asu.edu>
2023-05-23 21:22:35 -0700

    Make turn history container draggable

--- Assets/Scripts/Gameplay/UI/TurnHistoryContainer.cs
+++ Assets/Scripts/Gameplay/UI/TurnHistoryContainer.cs
@@ -1,18 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
-public class TurnHistoryContainer : MonoBehaviour
+public class TurnHistoryContainer : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler
 {
-    // Start is called before the first frame update
+    [SerializeField] private float _maximumYOffset;
+    private Vector3 _initialPosition;
+    private float _initialMouseY;
+
     void Start()
     {
-        
+        _initialPosition = transform.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void OnBeginDrag(PointerEventData eventData)
     {
-        
+        Debug.Log("Begin drag");
+        _initialMouseY = Input.mousePosition.y;
+
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+
+        float dragAmount = Input.mousePosition.y - _initialMouseY;
+        float newContainerY = Mathf.Clamp(_initialPosition.y + dragAmount, _initialPosition.y, _initialPosition.y + _maximumYOffset);
+
+transform.position = new Vector3(transform.position.x, newContainerY, transform.position.z);
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        transform.position = _initialPosition;
     }
 }
